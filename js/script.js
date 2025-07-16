31886d3d2cb4237e8cb6c4988b9e2b1eeac97860
tiziveg15 <vegatiziano015@gmail.com>
2025-07-15 22:18:03 -0300

    upgrade

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('searchBtn').addEventListener('click', () => {
+    alert("no sirve XD");
+});
+document.getElementById('searchInput').addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        alert("no sirve XD");
+    }
+})
